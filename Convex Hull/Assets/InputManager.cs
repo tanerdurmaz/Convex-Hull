6c52d39800dea117242b8b841aeb9e5d0def61d4
hexap <47399791+closedAi@users.noreply.github.com>
2021-05-04 16:40:37 +0300

    input manager updated
    camera Rotation and movement added + generated points now stored in an array

--- Convex Hull/Assets/InputManager.cs
+++ Convex Hull/Assets/InputManager.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-
+    float horizontalInput;
+    float verticalInput;
+    //float RotateHorizontalInput;
+    //float RotateVerticalInput;
+    float moveSpeed = 25f;
+    float rotationSpeed = 0.3f;
+    //Vector3 currentPosition;
+    //Vector3 inputVector;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +24,19 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ()
-        {
-            Debug.Log()
-        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //RotateHorizontalInput = Input.GetAxis("RotateHorizontal");
+        //RotateVerticalInput = Input.GetAxis("RotateVertical");
+
+
+
+        cam.transform.Translate(new Vector3(horizontalInput, Input.GetAxis("RotateVertical"), verticalInput) * moveSpeed * Time.deltaTime);
+
+        //currentPosition = cam.transform.position;
+
+        //inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        cam.transform.Rotate(0, Input.GetAxis("RotateHorizontal") * rotationSpeed, 0.0f);
     }
 }
--- Convex Hull/Assets/Scripts/PointGenerator.cs
+++ Convex Hull/Assets/Scripts/PointGenerator.cs
@@ -9,12 +9,14 @@ public class PointGenerator : MonoBehaviour
     [SerializeField] private float rangeY;
     [SerializeField] private float rangeZ;
     [SerializeField] private int pointCount;
+    [SerializeField] private GameObject[] points;
 
     // Start is called before the first frame update
     void Start()
     {
+        points = new GameObject[pointCount];
         for (int i = 0; i < pointCount; i++) { 
-            Instantiate(point, new Vector3(Random.Range(-1 * rangeX, rangeX), Random.Range(-1 * rangeY, rangeY), Random.Range(-1 * rangeZ, rangeZ)), Quaternion.identity);
+            points[i] = Instantiate(point, new Vector3(Random.Range(-1 * rangeX, rangeX), Random.Range(-1 * rangeY, rangeY), Random.Range(-1 * rangeZ, rangeZ)), Quaternion.identity);
         }
     }
 
